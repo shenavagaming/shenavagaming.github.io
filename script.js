96caacc76eebb90cc70b93ce070b286d13bb7241
Amrit Shenava <amritshenava98@users.noreply.github.com>
2018-10-08 07:31:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+  email = user.email;
+  emailVerified = user.emailVerified;  // The user's ID, unique to the Firebase project. Do NOT use
+                   // this value to authenticate with your backend server, if
+                   // you have one. Use User.getToken() instead.
+}
+
   } else {
     // No user is signed in.
+    window.prompt("You are not logged in");
   }
 });
 
@@ -23,9 +34,9 @@ function login(){
  var userEmail = document.getElementById("email_id").value;
  var userPass = document.getElementById("pass_id").value;
  
- window.prompt(userEmail + " " + userPass);
- //firebase.auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-  //var errorCode = error.code;
-  //var errorMessage = error.message;
- //});
+ 
+ firebase.auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+ });
 }
