7141d57159933a972bf051a004f9eda8c70f3bda
Amrit Shenava <amritshenava98@users.noreply.github.com>
2018-10-08 07:59:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,18 @@ function login(){
  var userPass = document.getElementById("pass_id").value;
  
  
- firebase.auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
- });
+         firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
 }
