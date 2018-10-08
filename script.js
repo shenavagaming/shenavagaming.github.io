1d5ab9713a4550dae40a951c51e4f5a6ce064a59
Amrit Shenava <amritshenava98@users.noreply.github.com>
2018-10-08 05:38:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function create_account(){
  var userEmail = document.getElementById("email_id").value;
  var userPass = document.getElementById("pass_id").value;
  
- window.alert(userEmail + " " + userPass);
+ 
  //firebase.auth.createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
    //var errorCode = error.code;
    //var errorMessage = error.message;
@@ -13,8 +13,9 @@ function login(){
  var userEmail = document.getElementById("email_id").value;
  var userPass = document.getElementById("pass_id").value;
  
- firebase.auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
+ window.alert(userEmail + " " + userPass);
+ //firebase.auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  //var errorCode = error.code;
+  //var errorMessage = error.message;
  });
 }
