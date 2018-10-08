24649a1cee11dba227804f65e686e30b36f2f7e5
Amrit Shenava <amritshenava98@users.noreply.github.com>
2018-10-08 05:17:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function create_account(){
 }
 
 function login(){
- var userEmail = document.getElementById().value;
- var userPass = document.getElementById().value;
+ var userEmail = document.getElementById("email_id").value;
+ var userPass = document.getElementById("pass_id").value;
  
  firebase.auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
   var errorCode = error.code;
